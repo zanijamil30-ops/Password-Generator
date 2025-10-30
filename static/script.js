12eb72c7b301aa372c95cc641ea7e95fb6940224
Zainab Jamil <zanijamil30@gmail.com>
2025-10-30 14:44:04 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,10 @@
+function copyPassword() {
+    const passwordText = document.getElementById("passwordText");
+    if (passwordText && passwordText.innerText.trim() !== "") {
+        navigator.clipboard.writeText(passwordText.innerText);
+        alert("Password copied to clipboard!");
+    } else {
+        alert("No password to copy!");
+    }
+}
 
